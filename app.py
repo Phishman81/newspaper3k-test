a4789c3cf7d1c671fe39edb207694855849705b9
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-19 23:23:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,65 +4,29 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-openai.api_key = 'your-openai-api-key'
+api_key = st.text_input("Enter your OpenAI API key", type="password")
 
-def scrape_website(url):
-    # Send a request to the website
-    r = requests.get(url)
+if api_key:
+    openai.api_key = api_key
 
-    # Get the content of the request
-    web_content = r.text
+    url = st.text_input("Enter the URL to scrape")
 
-    # Create a BeautifulSoup object and specify the parser
-    soup = BeautifulSoup(web_content, 'html.parser')
+    if url:
+        st.write(f"Scraping {url}...")
 
-    # This is a simple extraction of all text within paragraph tags
-    # Depending on the website structure, you might need to adjust this
-    text = ' '.join([p.text for p in soup.find_all('p')])
+        html_content = requests.get(url).text
+        soup = BeautifulSoup(html_content, "html.parser")
 
-    return text
+        text = ' '.join([p.text for p in soup.find_all('p')])
 
-st.title('Web Scraper App')
+        model_name = "gpt-4-0613"
 
-# User input for the URL
-url = st.text_input('Enter a URL')
+        response = openai.ChatCompletion.create(
+            model=model_name,
+            messages=[
+                {"role": "system", "content": f"You are a helpful assistant."},
+                {"role": "user", "content": text},
+            ],
+        )
 
-if url:
-    # Define the function
-    functions = [
-        {
-            "name": "scrape_website",
-            "description": "Scrape the content of a website",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "url": {
-                        "type": "string",
-                        "description": "The URL of the website"
-                    }
-                },
-                "required": ["url"]
-            }
-        }
-    ]
-
-    # Make the API call
-    response = openai.ChatCompletion.create(
-        model="gpt-4-0613",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"Scrape the content of this website: {url}"}
-        ],
-        functions=functions
-    )
-
-    # Extract the function call from the response
-    function_call = json.loads(response['choices'][0]['message']['function_call']['arguments'])
-
-    # Check if the function name matches and if the argument matches the provided URL
-    if function_call['name'] == 'scrape_website' and function_call['arguments']['url'] == url:
-        # Perform the actual web scraping
-        scraped_content = scrape_website(url)
-
-        st.subheader('Scraped Content:')
-        st.write(scraped_content)
+        st.write(response.choices[0].message['content'])
