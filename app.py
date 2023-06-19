f0d4c5c1bbd5b2ad362a37aef9d1f356ab8c2194
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-19 13:30:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+import streamlit as st
+from newspaper import Article
+
+def scrape_url(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.text
+
+st.title('URL Content Scraper')
+
+url = st.text_input('Enter a URL', '')
+
+if url:
+    st.write('Scraping...')
+
+    try:
+        text = scrape_url(url)
+        st.write(text)
+    except Exception as e:
+        st.write(f'Error: {e}')
 
